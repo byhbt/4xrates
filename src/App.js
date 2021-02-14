42f348b725f4730f5f8aed9f73d2a961f3c1c0c5
Thanh Huynh <huynhbathanh@gmail.com>
2021-02-14 13:32:23 +0700

    Implement the selection of currency

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import CurrencyRow from './CurrencyRow'
+
+const BASE_URL = 'https://api.exchangeratesapi.io/latest'
 
 function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+
+  const [fromCurrency, setFromCurrency] = useState([]);
+  const [toCurrency, setToCurrency] = useState([]);
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => {
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
+
+        const firstCurrency = Object.keys(data.rates)[0];
+        setFromCurrency(data.base);
+        setToCurrency(firstCurrency);
+      })
+  }, [])
+
   return (
-    <h1>Hello world</h1>
+    <>
+      <h1>4xRates</h1>
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={fromCurrency}
+        onChangeCurrency={e => setFromCurrency(e.target.value)}
+      />
+      <div>=</div>
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={toCurrency}
+        onChangeCurrency={e => setToCurrency(e.target.value)}
+      />
+    </>
   );
 }
 
